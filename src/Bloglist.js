ba75a49ccdf2a936ae01dfa6ff90d5dab4e8fd53
DevPeter <polalekan526@gmail.com>
2023-08-02 14:39:00 +0100

    works on useEffect

--- src/Bloglist.js
+++ src/Bloglist.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Bloglist = ({blog, name, handleDelete}) => {
+const Bloglist = ({blog, name}) => {
 // const blog = props.blog
 
     return (  
@@ -8,9 +8,9 @@ const Bloglist = ({blog, name, handleDelete}) => {
             <h2>{name}</h2>
         {blog.map((blog) => (    
            <div className="blog-preview" key = {blog.id}>
-                   <h2>{blog.dTtile}</h2>
-                   <p>{blog.dAuthour}</p>
-                   <button onClick={() => (handleDelete(blog.id) )}>Delete</button>
+                   <h2>{blog.title}</h2>
+                   <p>{blog.author}</p>
+                   
            </div>
     
         ))}
--- src/Dash.js
+++ src/Dash.js
@@ -4,28 +4,29 @@ import Bloglist from './Bloglist'
 
 
 const Dash = () => {
-  const [dBlogs, setdBlogs] = useState([
-    
-    {dAuthour: "Peter", dTtile: "Introduction to Meta Tags", dBody: "ajdjajdadjkads", id: 1 },
-    {dAuthour: "Wale", dTtile: "About Race condition to Meta Tags", dBody: "ajdjajdadjkads", id: 2},
-    {dAuthour: "John", dTtile: "The importance of well written code", dBody: "ajdjajdadjkads", id: 3},
-  ])
+  const [dBlogs, setdBlogs] = useState(null)
+  const [isPending, setIsPending]= useState(true)
 
-  const [name, setName] = useState("peter")
 
-  const handleDelete = (bId) =>{
-      const newBlogs = dBlogs.filter((blog) =>blog.id !== bId) 
-      setdBlogs(newBlogs)
-  } 
   useEffect(()=>{
-    console.log(dBlogs)
-  }, [name])
+    setTimeout(() => {
+      fetch("http://localhost:8000/dblogs")
+      .then(res => {
+        return res.json();
+      })
+      .then(data =>{
+        // console.log(dat)
+        setdBlogs(data);
+        setIsPending(false)
+      })
+    }, 100);
+   
+  }, []);
 
   return ( 
     <div className='home'>
-     <Bloglist blog = {dBlogs} name = "Major Blogs" handleDelete={handleDelete} />
-     <button onClick={() => setName("Tolulope")}> Click</button>
-     <p>{name}</p>
+      { isPending && <div style={{textAlign: "left"}}>Loading...</div>}
+     {dBlogs && <Bloglist blog = {dBlogs} name = "Major Blogs"  />}
     </div>
    );
 }
